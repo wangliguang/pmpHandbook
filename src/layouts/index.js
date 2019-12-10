f8b75433df60affb558d4e9d3c7e7ac6246f3265
王立广 <wangliguang@daling.com>
2019-12-10 10:05:33 +0800

    👍 FEAT: layout侧边栏的数据填充

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,6 @@
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import React from 'react';
+import LAYOUT_DATA from './layout';
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
@@ -20,43 +21,16 @@ export default class extends React.Component {
         <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
           <div className="logo" />
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1">
-              <Icon type="pie-chart" />
-              <span>Option 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="desktop" />
-              <span>Option 2</span>
-            </Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <Icon type="user" />
-                  <span>User</span>
-                </span>
-              }
-            >
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
+            {LAYOUT_DATA.map((item, i) => (
+              <SubMenu
+                key={`${i}`}
+                title={<span>{item.title}</span>}
+              >
+              {item.subType.map((subTypeTitle, j) => (
+                <Menu.Item key={`${i}_${j}`}>{subTypeTitle}</Menu.Item>
+              ))}
             </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={
-                <span>
-                  <Icon type="team" />
-                  <span>Team</span>
-                </span>
-              }
-            >
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9">
-              <Icon type="file" />
-              <span>File</span>
-            </Menu.Item>
+            ))}
           </Menu>
         </Sider>
         <Layout>
