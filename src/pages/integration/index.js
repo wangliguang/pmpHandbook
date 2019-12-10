5e98ac662dd8d73001fc6dab7d0fe148a7bf4b14
王立广 <wangliguang@daling.com>
2019-12-10 18:57:03 +0800

    每页内容分为：图片和文档和文字三类

--- src/pages/integration/index.js
+++ src/pages/integration/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import STYLE from './index.style.css';
+import { Collapse } from 'antd';
+const { Panel } = Collapse;
 
 const IMG_DATA = [
   require('../../../img/integration/1.png'),
@@ -7,19 +9,26 @@ const IMG_DATA = [
   require('../../../img/integration/3.png'),
   require('../../../img/integration/4.png'),
   require('../../../img/integration/5.png'),
+  require('../../../img/integration/6.png'),
+  require('../../../img/integration/7.png'),
+  require('../../../img/integration/8.png'),
 ]
 export default class IntegrationPage extends React.Component {
 
   render() {
     return (
       <div className={STYLE.page}>
-        {IMG_DATA.map((img) => (
-          <div className={STYLE.box}>
-            <div className={STYLE.pic}>
-              <img alt='' src={img}/>
-            </div>
-          </div>
-        ))}
+        <Collapse bordered={false} defaultActiveKey={['1']}>
+          <Panel header="图片" key="1">
+          {'xxx'}
+          </Panel>
+          <Panel header="This is panel header 2" key="2">
+            {'xxx'}
+          </Panel>
+          <Panel header="This is panel header 3" key="3">
+            {'xx'}
+          </Panel>
+        </Collapse>
       </div>
     )
   }
