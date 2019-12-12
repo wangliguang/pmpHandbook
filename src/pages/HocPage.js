262a15a014d4bf96b6dfeceff578c09cd56165c2
王立广 <wangliguang@daling.com>
2019-12-12 17:23:03 +0800

    fix: 统计代码

--- src/pages/HocPage.js
+++ src/pages/HocPage.js
@@ -21,6 +21,7 @@ export default function (component) {
     render() {
       return (
         <div style={{ padding: 20, background: "white"}}>
+          <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1278286627'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/z_stat.php%3Fid%3D1278286627' type='text/javascript'%3E%3C/script%3E"));</script>
           <Content imgs={this.state.imgData} files={this.state.fileData}/>
         </div>
       )
